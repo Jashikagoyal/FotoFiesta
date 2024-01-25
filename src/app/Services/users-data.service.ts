976663937ab5749b7c76888c082f078025ea2aec
Shubham Bajaj <shubhambajaj204@gmail.com>
2024-01-26 02:06:35 +0530

    Loading added to create event, removed font files,

--- src/app/Services/users-data.service.ts
+++ src/app/Services/users-data.service.ts
@@ -17,10 +17,10 @@ export class UsersDataService {
   activeSubscription$ = this._activeSubscriptionSubject.asObservable();
 
   set setUserDataInitial(response_data: any) {
-    console.log(" reponse data from Service" ,response_data)
+    // console.log(" reponse data from Service" ,response_data)
     this.responseData = response_data
     this.event_list = response_data.my_events
-    console.log("event List from service",this.event_list)
+    // console.log("event List from service",this.event_list)
     // this.activeSubscription = response_data.subscription
     this.activeSubscription = Array.isArray(this.responseData.subscription)
               ? this.responseData.subscription
@@ -40,15 +40,15 @@ export class UsersDataService {
           if (response) {
 
             this.responseData = response
-            console.log(" reponse data from Service", this.responseData)
+            // console.log(" reponse data from Service", this.responseData)
 
             this.event_list = this.responseData.my_events
-            console.log("event List from service", this.event_list)
+            // console.log("event List from service", this.event_list)
             this.activeSubscription = Array.isArray(this.responseData.subscription)
               ? this.responseData.subscription
               : [this.responseData.subscription];
             // this.activeSubscription = this.responseData.subscription
-            console.log("subscription from service", this.activeSubscription)
+            // console.log("subscription from service", this.activeSubscription)
             this._activeSubscriptionSubject.next(this.activeSubscription);
             
           }
